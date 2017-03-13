b2766b020386fbaa8346f1376769bc8b3fac32bf
Xunjie Zhou <xunjie.zhou@vodafone.com.au>
2017-03-13 10:16:44 +0800

    Add Spring MVC and JSP support for 17high.

--- src/main/java/com/tw/wh/devops/rest/controller/ActivitiesController.java
+++ src/main/java/com/tw/wh/devops/rest/controller/ActivitiesController.java
@@ -8,8 +8,6 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.websocket.server.PathParam;
-import java.util.List;
 
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
@@ -28,8 +26,7 @@ public class ActivitiesController {
                                   @RequestParam(defaultValue = "${17high.page.number}") int page,
                                   @RequestParam(required = false, defaultValue = "") String type,
                                   @RequestParam(required = false, defaultValue = "") String status,
-                                  @RequestParam(required = false, defaultValue = "") String sortBy,
-                                  @RequestParam(required = false, defaultValue = "") String sortOrder,
+                                  @RequestParam(required = false, defaultValue = "") String sort,
                                   HttpServletRequest request,
                                   HttpServletResponse response) {
 
---
+++ src/main/webapp/WEB-INF/jsp/addActivityForm.jsp
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+
+<%@ taglib prefix="spring" uri="http://www.springframework.org/tags"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+
+<html lang="en">
+<head>
+    <title>${title}</title>
+</head>
+<body>
+
+</body>
+
+</html>
