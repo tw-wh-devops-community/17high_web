69c7cc28c18f639c99ea8c36c52985ac50e4558c
weihongwei <hwwei@thoughtworks.com>
2017-03-15 13:52:53 +0800

    add react router

--- src/websrc/src/index.jsx
+++ src/websrc/src/index.jsx
@@ -1,7 +1,15 @@
-import React from 'react';
-import { render } from 'react-dom';
+import React from 'react'
+import { render } from 'react-dom'
+import { Router, Route, hashHistory } from 'react-router'
 import DemoComponent from './components/DemoComponent';
 
 const Demo = () => <DemoComponent />;
 
-render(<Demo />, document.getElementById('app'));
+render((
+  <Router history={hashHistory} queryKey="false">
+    <Route path="/" component={Demo}/>
+    <Route path="/repos" component={Demo}/>
+    <Route path="/about" component={Demo}/>
+  </Router>
+), document.getElementById('app'));
+
