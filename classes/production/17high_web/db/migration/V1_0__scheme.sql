01d5d18aa9fe2930aa30061482f628bedd8a7ebf
CooTWer <zryu@thoughtworks.com>
2017-03-21 11:21:00 +0800

    update code

---
+++ classes/production/17high_web/db/migration/V1_0__scheme.sql
@@ -0,0 +1,14 @@
+CREATE TABLE `activity` (
+`id` bigint(20) NOT NULL AUTO_INCREMENT,
+`end_time` datetime DEFAULT NULL,
+`imageurl` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+`location` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+`description` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+`name` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+`start_time` datetime DEFAULT NULL,
+`status` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+`sponsor` VARCHAR (255) COLLATE utf8mb4_unicode_ci DEFAULT NULL DEFAULT NULL,
+`guest` VARCHAR (255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+`type` VARCHAR (10),
+PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
