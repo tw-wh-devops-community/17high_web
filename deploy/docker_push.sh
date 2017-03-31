cda0259839d0ddcc055a8720c297530bf413e7d2
Yaowenjie <wjyao@thoughtworks.com>
2017-03-31 11:31:57 +0800

    deploy: move docker login into shell script and change port for app in compose

--- deploy/docker_push.sh
+++ deploy/docker_push.sh
@@ -2,7 +2,7 @@
 # Push only if it's not a pull request
 if [ -z "$TRAVIS_PULL_REQUEST" ] || [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
   # Push only if we're testing the master branch
-  if [ "$TRAVIS_BRANCH" == "deploy" ]; then
+  if [ "$TRAVIS_BRANCH" == "master" ]; then
 
     # This is needed to login on AWS and push the image on ECR
     # Change it accordingly to your docker repo
@@ -11,15 +11,12 @@ if [ -z "$TRAVIS_PULL_REQUEST" ] || [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
 #    eval $(aws ecr get-login --region $AWS_DEFAULT_REGION)
 
     # Build and push
+    docker login registry.cn-hangzhou.aliyuncs.com -u $ALI_DOCKER_USER -p $ALI_DOCKER_PASSWORD
     echo "building and pushing docker image"
     ./gradlew buildDocker
-#    docker build -t $IMAGE_NAME .
-#    echo "Pushing $IMAGE_NAME:latest"
-#    docker tag $IMAGE_NAME:latest "$REMOTE_IMAGE_URL:latest"
-#    docker push "$REMOTE_IMAGE_URL:latest"
-#    echo "Pushed $IMAGE_NAME:latest"
+    docker logout
   else
-    echo "Skipping deploy because branch is not 'deploy'"
+    echo "Skipping deploy because branch is not 'master'"
   fi
 else
   echo "Skipping deploy because it's a pull request"
