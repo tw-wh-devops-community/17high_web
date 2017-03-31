410c7d36e5640dc4e1e97ca229ee07238d764d0b
Yaowenjie <wjyao@thoughtworks.com>
2017-03-31 16:14:03 +0800

    deploy: trim cloudformation template.

--- deploy/env_preset.sh
+++ deploy/env_preset.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
 aws cloudformation create-stack --stack-name Wuhan17high --template-body file://./deploy/templates/ec2_instance.json
+aws cloudformation update-stack --stack-name Wuhan17high --template-body file://./deploy/templates/ec2_instance.json
+aws cloudformation delete-stack --stack-name Wuhan17high
 
 if ! type "git" > /dev/null; then
     sudo yum -y install git
@@ -28,8 +30,20 @@ if [ -d "17high_web" ]; then
     git checkout .
     git checkout deploy
     git pull origin deploy
+    export AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID}
+    export AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY}
+    docker-compose stop
+    docker-compose up -d
 else
     git clone https://github.com/tw-wh-devops-community/17high_web.git
     cd 17high_web
     git checkout deploy
+    export AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID}
+    export AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY}
+    docker-compose up -d
 fi
+
+
+
+
+
