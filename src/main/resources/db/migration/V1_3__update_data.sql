75d349a4562c6ee5f61a540b88b8c1bacb8e9e60
Yuqin1990 <Grace.shi1226@gmail.com>
2017-04-10 12:17:46 +0800

    Insert three columns into table activity

---
+++ src/main/resources/db/migration/V1_3__update_data.sql
@@ -0,0 +1,15 @@
+UPDATE activity SET create_time='2017-06-15 22:00:00' WHERE id=1;
+UPDATE activity SET create_time='2017-06-18 22:00:00' WHERE id=2;
+UPDATE activity SET create_time='2017-05-16 22:00:00' WHERE id=3;
+UPDATE activity SET create_time='2017-05-17 22:00:00' WHERE id=4;
+UPDATE activity SET create_time='2017-06-22 22:00:00' WHERE id=5;
+UPDATE activity SET create_time='2017-06-10 22:00:00' WHERE id=6;
+UPDATE activity SET create_time='2017-05-18 22:00:00' WHERE id=7;
+UPDATE activity SET create_time='2017-05-20 22:00:00' WHERE id=8;
+UPDATE activity SET create_time='2017-05-29 22:00:00' WHERE id=9;
+UPDATE activity SET create_time='2017-05-25 22:00:00' WHERE id=10;
+UPDATE activity SET create_time='2017-05-26 22:00:00' WHERE id=11;
+UPDATE activity SET create_time='2017-05-27 22:00:00' WHERE id=12;
+UPDATE activity SET create_time='2017-05-28 22:00:00' WHERE id=13;
+UPDATE activity SET create_time='2017-05-29 12:00:00' WHERE id=14;
+UPDATE activity SET create_time='2017-05-30 22:00:00' WHERE id=15;
