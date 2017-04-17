b78c00d5222b4f34ed2e150b9fda4308d5984274
Lu Chen <blacksea.chen@hotmail.com>
2017-04-17 16:33:06 +0800

    change default page to screen and remove demo component

--- src/websrc/src/index.jsx
+++ src/websrc/src/index.jsx
@@ -2,18 +2,16 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Router, Route, hashHistory } from 'react-router';
 import DashboardComponent from './components/DashboardComponent';
-import DemoComponent from './components/DemoComponent';
 import ScreenComponent from './components/ScreenComponent';
 import EventEditor from './components/EventEditor';
 
-const Demo = () => <DemoComponent />;
 const Screen = () => <ScreenComponent />;
 const Dashboard = () => <DashboardComponent />;
 const Editor = () => <EventEditor />;
 
 render((
   <Router history={hashHistory} queryKey="false">
-    <Route path="/" component={Demo} />
+    <Route path="/" component={Screen} />
     <Route path="/screen" component={Screen} />
     <Route path="/home" component={Dashboard} />
     <Route path="/editor" component={Editor} />
