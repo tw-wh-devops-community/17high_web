f8d214fdaf2c6782c66bc6606d8db544ba4375c5
Rui Sun <rsun@thoughtworks.com>
2017-08-10 16:00:19 +0800

    add react-intl and did one intl example

--- src/websrc/src/index.jsx
+++ src/websrc/src/index.jsx
@@ -1,18 +1,26 @@
-import React from "react";
-import {render} from "react-dom";
-import {Router, Route, hashHistory} from "react-router";
-import DashboardComponent from "./components/DashboardComponent";
-import ScreenComponent from "./components/ScreenComponent";
-import EventNewEditor from "./components/Editor/EventEditor";
+import React from 'react';
+import { render } from 'react-dom';
+import { Router, Route, hashHistory } from 'react-router';
+import { IntlProvider, addLocaleData } from 'react-intl';
+import localeZh from 'react-intl/locale-data/zh';
+import locales from './locale';
+import DashboardComponent from './components/DashboardComponent';
+import ScreenComponent from './components/ScreenComponent';
+import EventNewEditor from './components/Editor/EventEditor';
+
+addLocaleData([...localeZh]);
+
+const locale = 'zh';
 
 render((
-  <Router history={ hashHistory } queryKey="false">
-    <Route path="/" component={ ScreenComponent }/>
-    <Route path="/screen" component={ ScreenComponent }/>
-    <Route path="/home" component={DashboardComponent}/>
-    <Route path="/editor" component={EventNewEditor}>
-      <Route path=":id" component={ EventNewEditor }/>
-    </Route>
-  </Router>
+  <IntlProvider locale={locale} messages={locales[locale]}>
+    <Router history={ hashHistory } queryKey="false">
+      <Route path="/" component={ ScreenComponent } />
+      <Route path="/screen" component={ ScreenComponent } />
+      <Route path="/home" component={DashboardComponent} />
+      <Route path="/editor" component={EventNewEditor}>
+        <Route path=":id" component={ EventNewEditor } />
+      </Route>
+    </Router>
+  </IntlProvider>
 ), document.getElementById('app'));
-
---
+++ src/websrc/src/locale.jsx
@@ -0,0 +1,8 @@
+import ScreenZh from './components/ScreenComponent/locale/zh';
+
+const locals = {
+  zh: Object.assign({}, ScreenZh),
+  en: Object.assign({})
+};
+
+export default locals;
